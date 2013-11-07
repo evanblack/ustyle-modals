f2c88f49e9253843dfaa478adb072a397d17ced1
Craig Morey <craig.morey@uswitch.com>
2013-11-07 12:05:57 +0000

    release v0.5

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Ustyle modals
 
-Yet another modal dialog script, but made for uSwitch with the sort of options we need, and with excellent responsiveness.
+Yet another modal/dialog/pop-up/lightbox script, but made for uSwitch with the sort of options we need, and especially with responsiveness in mind.
 
-Inspired in a large way by CSS modals [drublic.github.io/css-modal/](http://drublic.github.io/css-modal/)
-
-THIS IS AN EARLY INCOMPLETE VERSION
+Inspired in a large way by CSS modals [drublic.github.io/css-modal/](http://drublic.github.io/css-modal/), but without the non-JS option (I know, I know).
 
 ## Why?
 
@@ -13,38 +11,36 @@ We used a bundle of different frameworks just for modals (bootstrap, jqueryUI, f
 ## Requirements
 
 - jQuery
-- SASS
-- spin.js [fgnass.github.io/spin.js/](http://fgnass.github.io/spin.js/)
-- (and uSwitch stylesheets/fonts in this iteration)
+- SASS (or compile the sass to css)
+- spin.js (optional [fgnass.github.io/spin.js/](http://fgnass.github.io/spin.js/))
+- uSwitch web and icon fonts (very optional)
 
 ## Installation
 
 include the js and css:
 
-    <link href='_uswitch_modal.css' media='all' rel='stylesheet' type='text/css' />
-    <script src='uSwitch.modal.js'></script>
+    <link href='_uswitch_modal.css' media='all' rel='stylesheet' type='text/css' /&gt;
+    <script src='uSwitch.modal.js'&gt;</script&gt;
 
-that's it. The javascript is auto-running and you're ready to build a modal pop-up
+that's it. The javascript is auto-running and you're ready to build a modal pop-up.
 
 ## Modes of use
-
-### Link triggered Non-JS
-
-Not yet tested, but just using CSS.
 
 ### Link triggered JS delegated event
 
 In this mode, you can just add a class to the link you want to trigger the modal:
 
-    <a href="/ajax-content.html" class="us-modal">click me</a>
+    <a href="/ajax-content.html" class="us-modal"&gt;click me</a&gt;
+
+The above will create a 600px wide responsive modal dialog filled with the contents of "/ajax-content.html"
 
 You can then add optional extra arguments in the form of data-types (see below for full list)
 
-    <a href="/ajax-content.html" class="us-modal" data-type="ajax" data-width="xl" data-title="Awesome pop-up" data-modalclass="green">click me</a>
+    <a href="/ajax-content.html" class="us-modal" data-type="ajax" data-width="xl" data-title="Awesome pop-up" data-modalclass="green"&gt;click me</a&gt;
 
 ### JS triggered
 
-You can also trigger a modal programmatically in JS, with exactly the same options
+You can also trigger a modal programmatically in JS, with exactly the same arguments
 
     uSwitch.modal.openModal(false,{
       'type':'ajax',
@@ -54,18 +50,20 @@ You can also trigger a modal programmatically in JS, with exactly the same optio
       'modalclass':'green'
     });
 
-## Options
+## Examples
 
-### Basic options
+[click here for lots of demos and examples](http://uswitch.github.io/ustyle-modals/)
+
+## Arguments
 
     modalid (default '#us-modal-0')
-By default there's only one modal scaffold in the page (with an id "us-modal-0"), which is automatically created on page load and has it's contents replaced each time a link is clicked. But this option allows the calling of multiple hidden prebuilt modals. Can be a CSS id "#this-modal" or the first available CSS class ".those-modals"
+Not usually specified, as by default there's only one modal scaffold in the page (with an id "us-modal-0"), which is automatically created on page load and has it's contents replaced each time a link is clicked. But this option allows the calling of multiple hidden prebuilt modals. Can be a CSS id "#this-modal" or the first available CSS class ".those-modals"
 
     modalclass
 Extra CSS classes to add to the modal (eg 'footer-green big-text')
 
     type (default 'prebuilt', but uses the url/target to determine the option, if empty)
-The type of content to pull into the modal. Choose from 'inpage' (pull a hidden element into the modal and force it as "display:block"), 'ajax' (pull content from a url), 'iframe' (NOT YET BUILT!) or 'prebuilt' (the modal is self-contained, prebuilt and hidden). Default is 'prebuilt', but the script will try and determine of it's 'inpage' or 'ajax' based on the target (ie the link) to pull into the modal.
+The type of content to pull into the modal. Choose from 'prebuilt' (the modal is self-contained, prebuilt and hidden), 'inpage' (pull a hidden element into the modal and force it as "display:block"), 'ajax' (pull content from a url) or 'iframe' (NOT YET BUILT!). Default is 'prebuilt', but the script will try and determine of it's 'inpage' or 'ajax' based on the target (ie the link) to pull into the modal.
 
     width (defaults to M for medium - 600px wide)
 Five widths are available: "XS,S,M,L,XL" ranging from 400px up to 800px wide. Why not allow arbitrary widths? Because then we'd have trouble spotting when the viewport is smaller than the modal and making them responsive without resorting to javascript events.
@@ -74,7 +72,7 @@ Five widths are available: "XS,S,M,L,XL" ranging from 400px up to 800px wide. Wh
 Only used for type="iframe". Otherwise the modal extends in height until it hits a maximum set in the stylesheet.
 
     target
-The content to pull into the modal (not used for type="prebuilt"). Can be set to "#nodeid" or ".nodeclass" for type="inpage" use, or 'http://www.bob.com/page.html' or '/page.html' or 'page.html' for type="ajax/iframe". Although it's crucial, it's also optional if you're triggering a link and data-target="url" is not set, as the link href="url" will be used instead.
+The content to pull into the modal (not used for type="prebuilt"). Can be set to "#nodeid" or ".nodeclass" for type="inpage/prebuilt" use, or 'http://www.bob.com/page.html' or '/page.html' or 'page.html' for type="ajax/iframe". Although it's crucial, it's also optional if you're triggering a link and data-target="url" is not set, as the href="url" will be used instead.
 
     targetextra
 Only used for type="ajax" calls. Allows you to specify an element within the document received. For example, target="test.html", targetextra="#bob" will only pull the first element with a CSS id "bob" from the document "test.html" and place it inside the modal
@@ -88,9 +86,12 @@ The text to add into the modal header. Yes it can be HTML if you really want
     noClose
 Set to 'true' if you'd prefer not to have a close box link in the modal header.
 
+    closedirection
+When you're viewing the modal on a small screen, the default behaviour is the same as desktop - a scale and fade opening transition. This option allows a slide transition to be substituted, with opions for "right" and "bottom"
+
 ### Footer options
 
-The modal footer doesn't appear by default, but if you specify it, it will appear fixed to the bottom of the modal.
+The modal footer doesn't appear by default, but if you specify it, it will appear fixed to the bottom of the modal. WARNING - the footer is always a single line height, it won't deal with long text running into multiple lines.
 
     showfooter
 If set to 'true', the modal footer will show. Duh. But then if "formurl" or "footerhtml" below are specified, this will also be set as 'true' by default.
@@ -108,7 +109,7 @@ The method of the form, defaults to 'POST'
 What to add into the left hand side of the form, eg "Click to switch".
 
     forminputs
-Ok this is funky. You can add multiple input fields in json form. eg
+A bit funky. You can add multiple input fields in a json object. eg
 {
  'field-name-1':{
   'type':'hidden',
@@ -127,16 +128,34 @@ Ok this is funky. You can add multiple input fields in json form. eg
 }
 
     formbutton
-Again, the button is in json form, if a little simpler:
+Again, the button is in a json object, if a little simpler:
 {
  'field-name-1':{
   'id':'blah',
   'class':'blah',
   'text':'blah'
  }
+
+## Modal scaffold
+
+If you want to prebuild a modal and put it in the page, here's an example. You don't need to add theoverlay, as that will be generated on "page ready".
+
+    &lt;section id="name-you-modal" class="us-modal-box us-modal-footer us-modal-xl" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true" style="display:none;"&gt;
+      &lt;header&gt;
+        &lt;h2 class="us-modal-title"&gt;This is a prebuilt modal&lt;/h2&gt;
+        &lt;a href="#!" class="us-modal-close"&gt;X&lt;/a&gt;
+      &lt;/header&gt;
+      &lt;div class="us-modal-content"&gt;
+        &lt;h2&gt;This is a "prebuilt" modal&lt;/h2&gt;
+        &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rhoncus urna vel nunc malesuada, sit amet lacinia augue vulputate. Donec suscipit porta mattis. Quisque varius leo et nibh fermentum, quis aliquam felis scelerisque. Quisque feugiat ligula nunc, in venenatis sem imperdiet at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut ultrices egestas commodo. Suspendisse non consequat odio.&lt;/p&gt;
+      &lt;/div&gt;
+      &lt;footer&gt;
+        &lt;p&gt;Footers here&lt;/p&gt;
+      &lt;/footer&gt;
+    &lt;/section&gt;
         
 ## Notes
 
-- All the arguments/variables are
+- All the arguments/variables keys are lowercase
 - need to fix on ipad. VH units seem to take the height of the page, not the viewport on iPad.
-- examples to come.
+- better aria work to come.
--- uSwitch.modal.js
+++ uSwitch.modal.js
@@ -1,21 +1,29 @@
+/* uswitch modals v0.5 - https://github.com/uswitch/ustyle-modals */
+
 var uSwitch = uSwitch || {};
 
 uSwitch.modal = (function() {
 
-  // v0.5
-
   var modalDefaultId = '#us-modal-0';
   var modalLinks = $('.us-modal');
+  var debug = false;
+
   
   var init = function(forceSetUp) {
+    if ($('html').hasClass('us-debug'))
+      debug = true;
+    if (debug)
+      console.log('-- us-modal init');
     setupModal(forceSetUp);
     setupClicks();
-  }
+  };
 
   // determine if there is a need for modals in the page
   var setupModal = function(forceSetUp) {
     // if we're asked to set-up with modals even if there's no modal links currently in the page, check one doesn't exist and set it up.
     if (forceSetUp && $('.us-modal-box').length==0) {
+      if (debug)
+        console.log('-- us-modal force setup');
       createModal();
       return;
     }
@@ -23,28 +31,33 @@ uSwitch.modal = (function() {
     // check the link of each modal
     $.each(modalLinks,function(index,el){
       var elJ = $(el);
-      var link = el.href;
+      var link = elJ.attr('href');
       // if this link isn't an internal anchor, it'll need a modal to be set-up
       if (link.indexOf('#')!=0 && link.length>1) {
         needWorkCounter++;
-      // if it is an internal anchor, check to see if it's already set-up, or if it's just any old internal anchor
+      // if it is an internal anchor, check to see if it's a 'prebuilt', or if it's just any old internal anchor
       } else {
         var anchor = link.substring(1);
-        if ($('.us-modal-container[id$="' + anchor + '"]').length == 0) {
+        if ($(link + '.us-modal-box').length === 0) {
           needWorkCounter++;
+        } else {
+          createOverlay();
+          if (debug)
+            console.log('-- us-modal prebuilt modal found in page - may not require scaffold setup');
         }
       }
-      // if needed, create a hidden modal ready for use
-      if (needWorkCounter > 0) {
-        createModal();
-      }
-    })
-  }
+    });
+    // if needed, create a hidden modal ready for use
+    if (needWorkCounter > 0) {
+      if (debug)
+        console.log('-- us-modal link found, scaffold setup required');
+      createModal();
+    }
+  };
 
   // build the scaffold of a modal hidden in the page
   var createModal = function() {
     var modalScaffold = '\
-    <div id="us-modal-overlay" class="us-modal-overlay" tabindex="-1" aria-hidden="true"></div>\
     <section id="' + modalDefaultId.replace('#','') + '" class="us-modal-box" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true">\
       <header>\
         <h2 class="us-modal-title"></h2>\
@@ -53,45 +66,67 @@ uSwitch.modal = (function() {
       <div class="us-modal-content"></div>\
       <footer></footer>\
     </section>';
+    createOverlay();
     $('body').append(modalScaffold);
-  }
+    if (debug)
+      console.log('-- us-modal setup modal scaffold');
+  };
+
+  var createOverlay = function() {
+    if ($('#us-modal-overlay').length === 0) {
+      $('body').append('<div id="us-modal-overlay" class="us-modal-overlay" tabindex="-1" aria-hidden="true"></div>');
+      if (debug)
+        console.log('-- us-modal setup overlay');
+    }
+  };
 
   // event handlers
   var setupClicks = function() {
     // links to open modals
     $('body').on('click','.us-modal',function(event) {
+      if (debug)
+        console.log('-- us-modal CLICK TO OPEN');
       event.preventDefault();
       openModal(this);
-    })
+    });
     // close modal link
     $('body').on('click','.us-modal-close',function() {
-      closeModal();
-    })
+      if (debug)
+        console.log('-- us-modal CLICK TO CLOSE');
+      closeModal($(this).closest('.us-modal-box'));
+    });
     // clicking the modal background mask also closes the modal
-    $('body').on('click','.us-modal-overlay',function(event) {
+    $('body').on('click','.us-modal-overlay',function() {
+      if (debug)
+        console.log('-- us-modal CLICK TO CLOSE FROM OVERLAY');
       closeModal();
-    })
-  }
+    });
+  };
 
   // helper that allows us to pass around both straight ids/classes (eg "#modal-signup", ".modal-signup") or jQuery objects (eg $('#modal-signup'))
   var jqOrId = function(undecided) {
+    var nodeJQ = false;
     if (undecided instanceof jQuery) {
-      var nodeJQ = undecided;
+      nodeJQ = undecided;
     } else {
       var nodeId = undecided || modalDefaultId;
-      var nodeJQ = $(nodeId);
+      nodeJQ = $(nodeId);
     }
     return nodeJQ;
-  }
+  };
 
-  // 
+  // read string encoded json, convert it to a json object
   var readJsonConfig = function(jsonRaw) {
     if (!jsonRaw)
       return;
-    jsonClean = jsonRaw.replace(/\'/g,'"');
-    jsonObj = JSON && JSON.parse(jsonClean) || $.parseJSON(jsonClean);
+    if (typeof jsonRaw === 'object')
+      return jsonRaw;
+    jsonRaw = jsonRaw.replace(/\"/g,'~~');  // mask double quotes
+    var jsonClean = jsonRaw.replace(/\'/g,'"');
+    jsonClean = jsonClean.replace(/\~~/g,'\"');
+    var jsonObj = JSON && JSON.parse(jsonClean) || $.parseJSON(jsonClean);
     return jsonObj;
-  }
+  };
 
   // if the config for the modal is in the form of data-type="" attributes on the link, read it into an object and fill in any important gaps
   var readLinkConfig = function(link) {
@@ -103,12 +138,13 @@ uSwitch.modal = (function() {
     }
     // fill in gaps: if we're not given a config.type, try and figure it out by the target given (ie the url or selector in the href)
     if (!config.type) {
-      if (config.target.indexOf('#') == 0 || config.target.indexOf('.') == 0) {
+      if (config.target.indexOf('#') === 0 || config.target.indexOf('.') === 0) {
         var contentJQ = $(config.target);
         if (contentJQ.length<1) {
           alert('sorry, unable to find the content you clicked on');
         } else if (contentJQ.hasClass('us-modal-box')) {
           config.type = 'prebuilt';
+          config.modalid = config.target;
         } else {
           config.type = 'inpage';
         }
@@ -120,96 +156,124 @@ uSwitch.modal = (function() {
     if (!config.title) {
       config.title = linkJQ[0].title || '';
     }
+    if (debug)
+      console.log('-- us-modal read modal config from link');
     return config;
-  }
+  };
 
   // clean the modal of content after it's closed
   var resetModal = function(noreset,modal) {
     var modaljq = jqOrId(modal);
-    var noreset = noreset || modaljq.data('noreset') || false;
+    noreset = noreset || modaljq.data('noreset') || false;
+    var showfooter = false;
+    if (modaljq.hasClass('us-modal-footer'))
+      showfooter = true;
     modaljq.attr('class','').addClass('us-modal-box');
     if (!noreset) {
       modaljq.find('.us-modal-title').text('');
       modaljq.find('.us-modal-content').text('');
       modaljq.find('footer').text('');
+      if (debug)
+        console.log('-- us-modal content reset');
+    } else if (showfooter) {
+      modaljq.addClass('us-modal-footer');
     }
-  }
+  };
 
   // set the modal title bar
   var setModalTitle = function(title,modal) {
     if (!title)
-      return
+      return;
     var modaljq = jqOrId(modal);
     modaljq.find('.us-modal-title').html(title);
-  }
+  };
 
   // set the modal content (type: inpage)
   var setModalContentInPage = function(targetjq,modal) {
     var modaljq = jqOrId(modal);
     var content = targetjq.first().clone().css('display','block');
     modaljq.find('.us-modal-content').append(content);
-  }
+  };
 
   // set the modal content (type: ajax)
   var setModalContentAjax = function(target,modal) {
     var modaljq = jqOrId(modal);
+    if (debug)
+      console.log('-- us-modal ajax target: ' + target);
     // add a spinner
     modaljq.find('.us-modal-content').append('<div id="us-modal-loading"></div>');
-    var opts = {
-      lines: 13, // The number of lines to draw
-      length: 6, // The length of each line
-      width: 3, // The line thickness
-      radius: 8, // The radius of the inner circle
-      corners: 1, // Corner roundness (0..1)
-      rotate: 0, // The rotation offset
-      direction: 1, // 1: clockwise, -1: counterclockwise
-      color: '#494B71', // #rgb or #rrggbb or array of colors
-      speed: 1, // Rounds per second
-      trail: 30, // Afterglow percentage
-      shadow: false,
-      hwaccel: false, // Whether to use hardware acceleration
-      zIndex: 2e9 // The z-index (defaults to 2000000000)
-    };
-    var spinTarget = $('#us-modal-loading')[0];
-    var spinner = new Spinner(opts).spin(spinTarget);
-    modaljq.find('.us-modal-content').load(target,function() {
+    if (typeof Spinner != "undefined") {
+      var opts = {
+        lines: 13, // The number of lines to draw
+        length: 6, // The length of each line
+        width: 3, // The line thickness
+        radius: 8, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#494B71', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 30, // Afterglow percentage
+        shadow: false,
+        hwaccel: false, // Whether to use hardware acceleration
+        zIndex: 2e9 // The z-index (defaults to 2000000000)
+      };
+      var spinTarget = $('#us-modal-loading')[0];
+      var spinner = new Spinner(opts).spin(spinTarget);
+    }
+    modaljq.find('.us-modal-content').load(target,function(responseText, textStatus, req) {
+      if (textStatus === "error") {
+        alert('sorry - we are unable to load the pop-up');
+        closeModal();
+        return;
+      }
       // remove spinner
       $('#us-spin-container').remove();
+      if (debug)
+        console.log('-- us-modal ajax complete');
     });
-  }
+  };
 
   // set the modal content (type: iframe)
   var setModalContentIframe = function(target,modal) {
     alert('NOT DONE!');
-  }
+  };
 
   var setModalFooter = function(config) {
     var config = config || {};
     var modalid = config.modalid || modalDefaultId;
     var modaljq = $(modalid);
-    var footerhtml = config.footerhtml || false;
+    var footerhtml = config.footerhtml || '';
     var formmethod = config.formmethod || 'POST';
     var formurl = config.formurl || false;
-    var formhtml = config.formhtml || false;
+    var formhtml = config.formhtml || '';
     var forminputs = readJsonConfig(config.forminputs) || {};
     var formbutton = readJsonConfig(config.formbutton) || {};
     formbutton.text = formbutton.text || 'submit';
+    if (debug) {
+      console.log('-- us-modal config with footer');
+      console.log(config);
+    }
     // form instead of html?
     if (!footerhtml && formurl) {
       footerhtml = '<form action="' + formurl + '" method="' + formmethod + '">';
       $.each(forminputs,function(i,el){
         footerhtml += '<input' + ( el['id'] ? ' id="' + el['id'] + '"':'') + ( el['class'] ? ' class="' + el['class'] + '"':'') + ' type="' + el.type + '" name="' + i + '" value="' + el.value + '" placeholder="' + el.placeholder + '" />';
-      })
+      });
       footerhtml += formhtml;
       footerhtml += '<button id="' + formbutton['id'] + '" class="btn ' + formbutton['class'] + '" type="submit">' + formbutton['text'] + '</button>';
       footerhtml += '</form>';
     }
     modaljq.find('footer').append(footerhtml);
-  }
+  };
 
   // open a modal
   var openModal = function(link,config) {
     var config = config || readLinkConfig(link) || {};
+    if (debug) {
+      console.log('-- us-modal first pass config:');
+      console.log(config);
+    }
     var modalid = config.modalid || modalDefaultId; // by default there's only one modal in the page. But this allows the option to call multiple hidden prebuilt modals.
     var modalclass = config.modalclass || false; // extra classes to add to the modal (eg 'footer-green big-text')
     var type = config.type || false; // the type of content to pull into the modal. Choose from 'inpage' (pull a hidden ID into the modal), 'ajax', 'iframe' or 'prebuilt' (the modal is self-contained, prebuilt and hidden). Default is 'prebuilt'.
@@ -219,8 +283,9 @@ uSwitch.modal = (function() {
     var targetextra = config.targetextra || false; // used in ajax requests, a CSS selector modifier that only retrieves a specific element of a page.
     var title = config.title || false; // title bar of modal
     var noclose = config.noclose || false; // hide the close button in the modal header. should be true or false
+    var closedirection = config.closedirection || false; // on small screens, what is the direction of close. default (false) is a scale fade (just like desktop). options are 'right' or 'bottom' to zoom the sheet off a side of the screen instead
     var formurl = config.formurl || false;
-    var footerhtml = config.footerhtml || false;
+    var footerhtml = config.footerhtml || '';
     // look to setModalFooter for additional config for setting up the modal footer
     var showfooter = config.showfooter || false; // show the modal footer. should be true or false
     if (!showfooter && (formurl || footerhtml)) // if we don't ask to show the footer, but do have a form or html, add the footer in anyway
@@ -228,11 +293,19 @@ uSwitch.modal = (function() {
     var noreset = config.noreset || false; // don't reset the modal when it's closed (ie, don't wipe any content). should be true or false
     var rootjq = $('html');
     var modaljq = $(modalid);
-    // let the modal know if it shouldn't have the data reset
+    // if we're looking at a prebuilt, make sure it doesn't have it's content wiped
+    if (type==='prebuilt')
+      noreset = true;
+    // let the modal know if it shouldn't have the data reset on close either
     if (noreset)
-      modaljq.data('noreset','1');
+      modaljq.attr('data-noreset','1');
+    if (debug) {
+      console.log('-- us-modal second pass config:');
+      console.log(config);
+    }
     // clear the modal
-    resetModal(noreset,modaljq);
+    if (!noreset)
+      resetModal(noreset,modaljq);
     // set title
     setModalTitle(title,modaljq);
     // set content
@@ -251,29 +324,37 @@ uSwitch.modal = (function() {
         setModalContentIframe(target,height,modaljq);
         break;
       default:
-        // do nothing about content, just reveal the modal (for use cases where the modal is pre-built but hidden - remember to use the "noreset" config setting)
+        // do nothing about content, just reveal the modal (for use cases where the modal is pre-built but hidden)
     }
     // set footer
     if (showfooter)
       setModalFooter(config,modaljq);
     // open modal/set the width
-    rootjq.addClass('us-modal-ready');
-    modaljq.addClass('us-modal-' + width + (noclose?' us-modal-noclose':'') + (showfooter?' us-modal-footer':'') + (modalclass?' ' + modalclass:''));
+    rootjq.addClass('us-modal-ready us-modal-root-' + width );
+    modaljq.addClass('us-modal-ready us-modal-' + width + (noclose?' us-modal-noclose':'') + (showfooter?' us-modal-footer':'') + (closedirection?' us-modal-dir-' + closedirection:'') + (modalclass?' ' + modalclass:''));
     setTimeout(function(){
       rootjq.removeClass('us-modal-ready').addClass('us-modal-on');
+      modaljq.removeClass('us-modal-ready').addClass('us-modal-on');
     },10);
-  }
+  };
 
   // close a modal
   var closeModal = function(modal) {
     var rootjq = $('html');
-    var modaljq = jqOrId(modal) || $(modalDefaultId);
-    rootjq.removeClass('us-modal-on').addClass('us-modal-ready');
+    var modaljq = $('.us-modal-box');
+    if (modal) {
+      modaljq = jqOrId(modal);
+    }
+    rootjq.removeClass(function (index, css) {
+      return (css.match (/\bus-modal-\S+/g) || []).join(' ');
+    });
+    modaljq.removeClass('us-modal-on').addClass('us-modal-ready');
     setTimeout(function(){
-      rootjq.removeClass('us-modal-ready');
+      rootjq.removeClass('us-modal-on us-modal-ready');
+      modaljq.removeClass('us-modal-on us-modal-ready');
       resetModal(false,modaljq);
     },400);
-  }
+  };
   
   return {
     init: init,
